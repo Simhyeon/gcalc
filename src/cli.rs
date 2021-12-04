4636e2f29e237a5c9ef044f828784c6dca6d0ce0
simoncreek <simoncreek@tutanota.com>
2021-12-04 18:14:57 +0900

    Some placeholders

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,5 @@
 use crate::GcalcResult;
-use clap::{ArgMatches,App};
+use clap::{ArgMatches,App, Arg};
 
 pub struct Cli;
 
@@ -14,7 +14,14 @@ impl Cli {
         App::new("gclac")
             .version("0.1.0")
             .author("Simon creek <simoncreek@tutanota.com>")
-            .about("Gcalc is a gacha simulator for game development and other decision makings.")
+            .about("Gcalc is a gacha simulator for game development and other decision makings.") // meta information end
+            .subcommand(
+                App::new("range")
+                .arg(Arg::new("PROB").required(true).about("Target probabilty"))
+                .arg(Arg::new("count").required(true).about("Counts to execute"))
+                .arg(Arg::new("start").about("Starting index to print"))
+                .arg(Arg::new("").about("Table format"))
+            ) // range subcommand
             .get_matches()
     }
 
