5b116c4ce9f1958c87578073ff92bc84b940a723
simoncreek <simoncreek@tutanota.com>
2021-12-04 18:14:10 +0900

    New error types

--- src/error.rs
+++ src/error.rs
@@ -5,5 +5,17 @@ pub enum GcalcError {
     #[error("Foramtting fail : {0}")]
     FormatFail(Box<dyn std::error::Error>),
     #[error("Invalid argument : {0}")]
-    InvalidArgument(String)
+    InvalidArgument(String),
+    #[error("Standard IO error\n= {0}")]
+    StdIo(std::io::Error),
+    #[error("Invalid csv error\n= {0}")]
+    CsvError(String),
+    #[error("Invalid probabilty form\n= {0}")]
+    InvalidProb(String),
+}
+
+impl From<std::io::Error> for GcalcError {
+    fn from(err : std::io::Error) -> Self {
+        Self::StdIo(err)
+    }
 }
