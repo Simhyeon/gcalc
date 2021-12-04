af92ee07a765523aae7b707397da2b3c742d9d7e
simoncreek <simoncreek@tutanota.com>
2021-12-04 18:14:46 +0900

    Fixed prob fromatting + added enw sanity check methodg

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,23 @@
-use crate::calc::ProbType;
+use crate::{calc::ProbType, GcalcResult, GcalcError};
 
 /// Get probabilty according to given type
 ///
 /// Available types are
 /// - Percentage
 /// - Floating number
-pub fn get_prob_as_type(num: f32, prob_type: &ProbType, precision: &Option<usize>) -> String {
-    let mut num = float_to_string(num, precision);
+pub fn get_prob_as_type(mut num: f32, prob_type: &ProbType, precision: &Option<usize>) -> String {
+    let mut num_string: String;
 
     // Add percentage
     if let ProbType::Percentage = prob_type { 
-        num.push_str("%");
+        num = num * 100f32; // multiply by 100
+        num_string = float_to_string(num, precision);
+        num_string.push_str("%");
+    } else {
+        num_string = float_to_string(num, precision);
     }
-    num
+
+    num_string
 }
 
 /// Convert floating number to string 
@@ -23,5 +28,13 @@ pub fn float_to_string(num: f32, precision: &Option<usize>) -> String {
         format!("{:.1$}",num,precision)
     } else {
         format!("{}",num)
+    }
+}
+
+pub fn prob_sanity_check(num: f32) -> GcalcResult<()> {
+    if num > 1.0f32 {
+        return Err(GcalcError::InvalidProb(format!("Given number : {} is bigger than 1.0", num)));
+    } else {
+        Ok(())
     }
 }
