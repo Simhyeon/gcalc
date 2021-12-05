29e5801426f52c141721173dd0cec9e56ff5d3a7
simoncreek <simoncreek@tutanota.com>
2021-12-06 03:19:20 +0900

    New subcommand + made code DRY

--- src/cli.rs
+++ src/cli.rs
@@ -11,21 +11,33 @@ impl Cli {
     }
 
     fn args_builder() -> ArgMatches {
-        App::new("gclac")
+        App::new("gcalc")
             .version("0.1.0")
             .author("Simon creek <simoncreek@tutanota.com>")
             .about("Gcalc is a gacha simulator for game development and other decision makings.") // meta information end
             .subcommand(
+                App::new("cond")
+                .arg(Arg::new("PROB").about("Basic probabilty").takes_value(true))
+                .arg(Arg::new("reference").about("Reference file").short('r').long("ref").takes_value(true))
+                .arg(Arg::new("budget").about("Budget of total cost").short('b').long("budget").takes_value(true))
+                .arg(Arg::new("target").about("Target probabilty").short('t').long("target").takes_value(true))
+                .arg(Arg::new("format").about("Table format").short('f').long("format").takes_value(true))
+                .arg(Arg::new("precision").about("Precision").short('p').long("precision").takes_value(true))
+                .arg(Arg::new("probtype").about("Probabilty type").short('T').long("type").takes_value(true))
+                .arg(Arg::new("cost").about("Cost per try").short('C').long("cost").takes_value(true))
+            )
+            .subcommand(
                 App::new("range")
-                .arg(Arg::new("PROB").required(true).about("Target probabilty").takes_value(true))
+                .arg(Arg::new("PROB").about("Basic probabilty").takes_value(true))
                 .arg(Arg::new("reference").about("Reference file").short('r').long("ref").takes_value(true))
                 .arg(Arg::new("count").required(true).about("Counts to execute").short('c').long("count").takes_value(true))
                 .arg(Arg::new("start").about("Starting index to print").short('s').long("start").takes_value(true))
                 .arg(Arg::new("format").about("Table format").short('f').long("format").takes_value(true))
                 .arg(Arg::new("precision").about("Precision").short('p').long("precision").takes_value(true))
-                .arg(Arg::new("probtype").about("Probabilty type").short('t').long("type").takes_value(true))
-            ) // range subcommand
-            .subcommand(App::new("reference"))
+                .arg(Arg::new("probtype").about("Probabilty type").short('T').long("type").takes_value(true))
+                .arg(Arg::new("cost").about("Cost per try").short('C').long("cost").takes_value(true))
+            ) // "range" subcommand
+            .subcommand(App::new("reference")) // "reference" file creation subcommand
             .get_matches()
     }
 
@@ -33,6 +45,9 @@ impl Cli {
         match args.subcommand() {
             Some(( "range" , range_m)) => {
                 Self::subcommand_range(range_m)?;
+            }
+            Some(( "cond" , cond_m)) => {
+                Self::subcommand_conditional(cond_m)?;
             }
             Some(( "reference" , _)) => {
                 Self::subcommand_reference()?;
@@ -44,18 +59,59 @@ impl Cli {
     }
 
     fn subcommand_range(args: &ArgMatches) -> GcalcResult<()> {
-        let probabilty = args.value_of("PROB").unwrap().parse().expect("Probabilty should be float");
-        utils::prob_sanity_check(probabilty)?;
-        let count = args.value_of("count").unwrap().parse().expect("count should be integer");
-        let mut cal = Calculator::new(probabilty,count)?;
+        let probability = Self::get_sane_probability(args)?;
+        let count = args.value_of("count")
+            .unwrap()
+            .parse()
+            .expect("count should be an integer");
+
+        let mut cal = Calculator::new(probability)?;
         let mut min = 0;
+        if let Some(index) = args.value_of("start") {
+            min = index.parse().expect("Failed to get precisino as usize");
+        }
+        Self::set_calculator_attribute(&mut cal, args)?;
+
+        cal.print_range(count, Some(min))?;
+
+        Ok(())
+    }
+
+    fn subcommand_conditional(args: &ArgMatches) -> GcalcResult<()> {
+        let prob = Self::get_sane_probability(args)?;
+        let mut cal = Calculator::new(prob)?;
+        Self::set_calculator_attribute(&mut cal, args)?;
+
+        if let Some(target) = args.value_of("target") {
+            cal.set_target_probability(target.parse().expect("Failed to get target prob"));
+        }
+
+        if let Some(budget) = args.value_of("budget") {
+            cal.set_budget(budget.parse().expect("Failed to get budget"));
+        }
+
+        cal.print_conditional()?;
+        Ok(())
+    }
+
+    // Utils, DRY codes
+
+    fn get_sane_probability(args: &ArgMatches) -> GcalcResult<f32> {
+        let probabilty = args.value_of("PROB")
+            .unwrap_or("1.0")
+            .parse()
+            .expect("Probabilty should be float");
+        utils::prob_sanity_check(probabilty)?;
+        Ok(probabilty)
+    }
+
+    fn set_calculator_attribute(cal : &mut Calculator, args: &ArgMatches) -> GcalcResult<()> {
+        if let Some(cost) = args.value_of("cost") {
+            cal.set_cost(cost.parse().expect("Failed to get cost"));
+        }
 
         if let Some(csv_file) = args.value_of("reference") {
             cal.set_csv_file(std::path::Path::new(csv_file));
-        }
-
-        if let Some(start) = args.value_of("start") {
-            min = start.parse().expect("Start should be integer");
         }
 
         if let Some(format) = args.value_of("format") {
@@ -69,8 +125,6 @@ impl Cli {
         if let Some(prob_type) = args.value_of("probtype") {
             cal.set_prob_type(ProbType::from_str(prob_type)?);
         }
-
-        cal.print_range(Some((min,count)))?;
         Ok(())
     }
 
