005a4f1640d3c59a5e1872e043840e4feaa7d599
simoncreek <simoncreek@tutanota.com>
2021-12-07 22:19:38 +0900

    Added new mod

---
+++ src/consts.rs
@@ -0,0 +1,6 @@
+pub const COUNT_INDEX       : usize = 0;
+pub const BASIC_PROB_INDEX  : usize = 1;
+pub const ADDED_PROB_INDEX  : usize = 2;
+pub const COST_INDEX        : usize = 3;
+
+pub const COLUMN_LEN        : usize = 4;
