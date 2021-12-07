84317e679812fd517c05b78cc143f112e6382f00
simoncreek <simoncreek@tutanota.com>
2021-12-07 22:23:45 +0900

    New models

--- src/models.rs
+++ src/models.rs
@@ -1,2 +1,59 @@
+use std::path::PathBuf;
+
+use serde::Serialize;
+use tabled::Tabled;
+
 use crate::GcalcError;
 pub type GcalcResult<T> = Result<T, GcalcError>;
+
+pub struct ColumnMap {
+    pub count: usize,
+    pub probability: usize,
+    pub added: usize,
+    pub cost: usize,
+}
+
+impl ColumnMap {
+    pub fn new( count: usize, probability: usize, added: usize, cost: usize) -> Self {
+        Self { count, probability, added, cost, }
+    }
+}
+
+#[derive(Serialize, Tabled)]
+pub(crate) struct Qualficiation {
+    pub count: usize,
+    pub cost : f32,
+}
+
+impl Qualficiation {
+    pub fn new(count: usize, cost: f32) -> Self {
+        Self {
+            count,
+            cost,
+        }
+    }
+}
+
+#[derive(Serialize, Tabled)]
+pub(crate) struct Record {
+    pub count: usize,
+    pub probability : String,
+    pub cost : f32,
+}
+
+impl Record {
+    pub fn new(count: usize, probability: String, cost: f32) -> Self {
+        Self {
+            count,
+            probability,
+            cost,
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum RefCsv {
+    Raw(String),
+    File(PathBuf),
+    None,
+}
