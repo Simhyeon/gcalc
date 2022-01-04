992fd0a74033c1fc17f19812627ff33c152854e8
simoncreek <simoncreek@tutanota.com>
2022-01-04 15:00:09 +0900

    Force export

--- www/index.js
+++ www/index.js
@@ -1,11 +1,15 @@
-import * as wasm from "gcalc";
+import * as gcalc from "gcalc";
 
-let option = wasm.default_option();
-option.prob = 0.1;
-option.count = 10;
-option.cost = 100
-option.prob_precision = 2;
-let csv = wasm.calculate("range", option);
-document.querySelector("#content").textContent = csv;
+class Calculator {
+	constructor() {
+		this.option = gcalc.default_option();
+		this.csv = "";
+	}
 
-export let csv_value = csv;
+	calculate() {
+		this.csv = gcalc.calculate("range", this.option);
+	}
+}
+
+// Force export class to browser
+window.Calculator = Calculator;
