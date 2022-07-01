03d4f27d65b5b956bd1728f92c743497e8d1aa81
simoncreek <simoncreek@tutanota.com>
2022-07-02 00:43:44 +0900

    Version up

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,10 @@
-use crate::{GcalcResult, Calculator, utils, TableFormat, ProbType, models::{CsvRef}, GcalcError, calc::CalculatorOption};
-use clap::{ArgMatches,App, Arg};
-use std::{path::PathBuf, io::Read, collections::HashMap, iter::FromIterator};
+use crate::{
+    calc::CalculatorOption, models::CsvRef, utils, Calculator, GcalcError, GcalcResult, ProbType,
+    TableFormat,
+};
+use clap::{App, Arg, ArgMatches};
 use std::io::Write;
+use std::{collections::HashMap, io::Read, iter::FromIterator, path::PathBuf};
 
 pub struct Cli;
 
@@ -13,75 +16,208 @@ impl Cli {
     }
 
     fn args_builder() -> ArgMatches {
-
-        let cond_app  = Self::common_app_args(App::new("cond").about("Conditional calculation"));
-        let range_app = Self::common_app_args(App::new("range").about("Prints range of calculations"));
-        let qual_app  = Self::common_app_args(App::new("qual").about("Conditional calculation but only prints result"));
+        let cond_app = Self::common_app_args(App::new("cond").about("Conditional calculation"));
+        let range_app =
+            Self::common_app_args(App::new("range").about("Prints range of calculations"));
+        let qual_app = Self::common_app_args(
+            App::new("qual").about("Conditional calculation but only prints result"),
+        );
 
         let main_app = App::new("gcalc")
-            .version("0.3.1")
+            .version("0.4.0")
             .author("Simon creek <simoncreek@tutanota.com>")
             .about("Gcalc is a gacha simulator for game development and other decision makings.") // meta information end
-            .subcommand(cond_app
-                .arg(Arg::new("budget").help("Budget of total cost").short('b').long("budget").takes_value(true))
-                .arg(Arg::new("target").help("Target probability").short('t').long("target").takes_value(true))
-                .arg(Arg::new("offset").help("Record offset").long("offset").takes_value(true))
-                .arg(Arg::new("plot").help("Crate plot chart").long("plot"))
-            ).subcommand(qual_app
-                .arg(Arg::new("budget").help("Budget of total cost").short('b').long("budget").takes_value(true))
-                .arg(Arg::new("target").help("Target probability").short('t').long("target").takes_value(true))
-            ).subcommand(range_app 
-                .arg(Arg::new("count").help("Counts to execute").short('c').long("count").takes_value(true))
-                .arg(Arg::new("start").help("Starting index to print").short('S').long("start").takes_value(true))
-                .arg(Arg::new("plot").help("Crate plot chart").long("plot"))
-            ).subcommand(App::new("reference").about("Create a reference file"));
-
-            #[cfg(feature = "option")]
-            let app = main_app.subcommand(App::new("option").about("Create an option file")); // "option" file creation subcommand
-
-            app.get_matches()
-    }
-
-    fn common_app_args(app : clap::App) -> clap::App {
-        let app = app.arg(Arg::new("prob").help("Basic probability").short('p').long("probability").takes_value(true))
-            .arg(Arg::new("cost").help("Cost per try").short('C').long("cost").takes_value(true))
-            .arg(Arg::new("constant").help("Constant value to be added into probability").long("constant").takes_value(true))
-            .arg(Arg::new("reference").help("Reference file").short('r').long("ref").takes_value(true).conflicts_with("refin"))
-            .arg(Arg::new("refin").help("Reference from stdin").long("refin").conflicts_with("reference"))
-            .arg(Arg::new("format").help("Table format(csv|console|gfm)").short('f').long("format").takes_value(true).default_value("console"))
-            .arg(Arg::new("precision").help("Precision").short('P').long("precision").takes_value(true).default_value("2"))
-            .arg(Arg::new("probtype").help("Probability type(percentage|fraction)").short('T').long("type").takes_value(true))
-            .arg(Arg::new("column").help("Column mapping").long("column").takes_value(true))
-            .arg(Arg::new("noheader").help("CSV without header").long("noheader"))
-            .arg(Arg::new("out").help("Out file").short('o').long("out").takes_value(true))
-            .arg(Arg::new("fallback").help("Set csv value fallback (rollback|ignore|none)").long("fallback").default_value("none"))
-            .arg(Arg::new("strict").help("Set strict CSV reader mode").short('s').long("strict"));
+            .subcommand(
+                cond_app
+                    .arg(
+                        Arg::new("budget")
+                            .help("Budget of total cost")
+                            .short('b')
+                            .long("budget")
+                            .takes_value(true),
+                    )
+                    .arg(
+                        Arg::new("target")
+                            .help("Target probability")
+                            .short('t')
+                            .long("target")
+                            .takes_value(true),
+                    )
+                    .arg(
+                        Arg::new("offset")
+                            .help("Record offset")
+                            .long("offset")
+                            .takes_value(true),
+                    )
+                    .arg(Arg::new("plot").help("Crate plot chart").long("plot")),
+            )
+            .subcommand(
+                qual_app
+                    .arg(
+                        Arg::new("budget")
+                            .help("Budget of total cost")
+                            .short('b')
+                            .long("budget")
+                            .takes_value(true),
+                    )
+                    .arg(
+                        Arg::new("target")
+                            .help("Target probability")
+                            .short('t')
+                            .long("target")
+                            .takes_value(true),
+                    ),
+            )
+            .subcommand(
+                range_app
+                    .arg(
+                        Arg::new("count")
+                            .help("Counts to execute")
+                            .short('c')
+                            .long("count")
+                            .takes_value(true),
+                    )
+                    .arg(
+                        Arg::new("start")
+                            .help("Starting index to print")
+                            .short('S')
+                            .long("start")
+                            .takes_value(true),
+                    )
+                    .arg(Arg::new("plot").help("Crate plot chart").long("plot")),
+            )
+            .subcommand(App::new("reference").about("Create a reference file"));
 
         #[cfg(feature = "option")]
-        let app = app.arg(Arg::new("option").help("Option file to use").short('O').long("option").takes_value(true));
+        let app = main_app.subcommand(App::new("option").about("Create an option file")); // "option" file creation subcommand
+
+        app.get_matches()
+    }
+
+    fn common_app_args(app: clap::App) -> clap::App {
+        let app = app
+            .arg(
+                Arg::new("prob")
+                    .help("Basic probability")
+                    .short('p')
+                    .long("probability")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::new("cost")
+                    .help("Cost per try")
+                    .short('C')
+                    .long("cost")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::new("constant")
+                    .help("Constant value to be added into probability")
+                    .long("constant")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::new("reference")
+                    .help("Reference file")
+                    .short('r')
+                    .long("ref")
+                    .takes_value(true)
+                    .conflicts_with("refin"),
+            )
+            .arg(
+                Arg::new("refin")
+                    .help("Reference from stdin")
+                    .long("refin")
+                    .conflicts_with("reference"),
+            )
+            .arg(
+                Arg::new("format")
+                    .help("Table format(csv|console|gfm)")
+                    .short('f')
+                    .long("format")
+                    .takes_value(true)
+                    .default_value("console"),
+            )
+            .arg(
+                Arg::new("precision")
+                    .help("Precision")
+                    .short('P')
+                    .long("precision")
+                    .takes_value(true)
+                    .default_value("2"),
+            )
+            .arg(
+                Arg::new("probtype")
+                    .help("Probability type(percentage|fraction)")
+                    .short('T')
+                    .long("type")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::new("column")
+                    .help("Column mapping")
+                    .long("column")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::new("noheader")
+                    .help("CSV without header")
+                    .long("noheader"),
+            )
+            .arg(
+                Arg::new("out")
+                    .help("Out file")
+                    .short('o')
+                    .long("out")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::new("fallback")
+                    .help("Set csv value fallback (rollback|ignore|none)")
+                    .long("fallback")
+                    .default_value("none"),
+            )
+            .arg(
+                Arg::new("strict")
+                    .help("Set strict CSV reader mode")
+                    .short('s')
+                    .long("strict"),
+            );
+
+        #[cfg(feature = "option")]
+        let app = app.arg(
+            Arg::new("option")
+                .help("Option file to use")
+                .short('O')
+                .long("option")
+                .takes_value(true),
+        );
 
         app
     }
 
     fn run_calculator(args: &ArgMatches) -> GcalcResult<()> {
         match args.subcommand() {
-            Some(( "range" , range_m)) => {
+            Some(("range", range_m)) => {
                 Self::subcommand_range(range_m)?;
             }
-            Some(( "cond" , cond_m)) => {
+            Some(("cond", cond_m)) => {
                 Self::subcommand_conditional(cond_m)?;
             }
-            Some(( "qual" , qual_m)) => {
+            Some(("qual", qual_m)) => {
                 Self::subcommand_qual(qual_m)?;
             }
-            Some(( "reference" , _)) => {
+            Some(("reference", _)) => {
                 Self::subcommand_reference()?;
             }
             #[cfg(feature = "option")]
-            Some(( "option" , _)) => {
+            Some(("option", _)) => {
                 Self::subcommand_option()?;
             }
-            _ => writeln!(std::io::stderr(),"No proper sub command was given to the program")?,
+            _ => writeln!(
+                std::io::stderr(),
+                "No proper sub command was given to the program"
+            )?,
         }
 
         Ok(())
@@ -90,22 +226,26 @@ impl Cli {
     fn subcommand_range(args: &ArgMatches) -> GcalcResult<()> {
         // Override count if value was given
         let count = if let Some(count) = args.value_of("count") {
-            Some(count.parse::<usize>().map_err( |_| GcalcError::ParseError("Count should be a positive integer".to_owned()))?)
-        } else { None };
+            Some(count.parse::<usize>().map_err(|_| {
+                GcalcError::ParseError("Count should be a positive integer".to_owned())
+            })?)
+        } else {
+            None
+        };
 
         let mut cal = Calculator::new()?;
         let mut min = 0;
         if let Some(index) = args.value_of("start") {
-            min = index.parse()
-                .map_err( |_| GcalcError::ParseError("Start index should be a positive integer (usize)".to_owned()))?;
+            min = index.parse().map_err(|_| {
+                GcalcError::ParseError(
+                    "Start index should be a positive integer (usize)".to_owned(),
+                )
+            })?;
         }
         Self::set_calculator_attribute(&mut cal, args)?;
 
         cal.set_plot(args.is_present("plot"));
-        cal.print_range(
-            count, 
-            Some(min),
-        )?;
+        cal.print_range(count, Some(min))?;
 
         Ok(())
     }
@@ -115,17 +255,23 @@ impl Cli {
         Self::set_calculator_attribute(&mut cal, args)?;
 
         if let Some(target) = args.value_of("target") {
-            let prob = target.parse().map_err( |_| GcalcError::ParseError("Target should be a float within 0.0 ~ 1.0".to_owned()))?;
+            let prob = target.parse().map_err(|_| {
+                GcalcError::ParseError("Target should be a float within 0.0 ~ 1.0".to_owned())
+            })?;
             cal.set_target_probability(prob)?;
         }
 
         if let Some(budget) = args.value_of("budget") {
-            let budget = budget.parse().map_err( |_| GcalcError::ParseError("Budget should be a number".to_owned()))?;
+            let budget = budget
+                .parse()
+                .map_err(|_| GcalcError::ParseError("Budget should be a number".to_owned()))?;
             cal.set_budget(budget);
         }
 
         if let Some(offset) = args.value_of("offset") {
-            let offset = offset.parse().map_err( |_| GcalcError::ParseError("Budget should be a number".to_owned()))?;
+            let offset = offset
+                .parse()
+                .map_err(|_| GcalcError::ParseError("Budget should be a number".to_owned()))?;
             cal.set_offset(offset);
         }
 
@@ -139,12 +285,16 @@ impl Cli {
         Self::set_calculator_attribute(&mut cal, args)?;
 
         if let Some(target) = args.value_of("target") {
-            let prob = target.parse().map_err( |_| GcalcError::ParseError("Target should be a float within 0.0 ~ 1.0".to_owned()))?;
+            let prob = target.parse().map_err(|_| {
+                GcalcError::ParseError("Target should be a float within 0.0 ~ 1.0".to_owned())
+            })?;
             cal.set_target_probability(prob)?;
         }
 
         if let Some(budget) = args.value_of("budget") {
-            let budget = budget.parse().map_err( |_| GcalcError::ParseError("Budget should be a number".to_owned()))?;
+            let budget = budget
+                .parse()
+                .map_err(|_| GcalcError::ParseError("Budget should be a number".to_owned()))?;
             cal.set_budget(budget);
         }
 
@@ -152,10 +302,10 @@ impl Cli {
         Ok(())
     }
 
-    fn set_calculator_attribute(cal : &mut Calculator, args: &ArgMatches) -> GcalcResult<()> {
+    fn set_calculator_attribute(cal: &mut Calculator, args: &ArgMatches) -> GcalcResult<()> {
         #[cfg(feature = "option")]
         if let Some(file) = args.value_of("option") {
-            let mut option =  CalculatorOption::from_file(std::path::Path::new(file))?;
+            let mut option = CalculatorOption::from_file(std::path::Path::new(file))?;
             cal.set_option(&mut option);
         }
 
@@ -165,12 +315,16 @@ impl Cli {
         }
 
         if let Some(cost) = args.value_of("cost") {
-            let cost = cost.parse().map_err( |_| GcalcError::ParseError("Cost should be a number".to_owned()))?;
+            let cost = cost
+                .parse()
+                .map_err(|_| GcalcError::ParseError("Cost should be a number".to_owned()))?;
             cal.set_cost(cost, true);
         }
 
         if let Some(cost) = args.value_of("constant") {
-            let constant = cost.parse().map_err( |_| GcalcError::ParseError("Constant should be a number".to_owned()))?;
+            let constant = cost
+                .parse()
+                .map_err(|_| GcalcError::ParseError("Constant should be a number".to_owned()))?;
             cal.set_constant(constant, true)?;
         }
 
@@ -189,8 +343,9 @@ impl Cli {
         }
 
         if let Some(precision) = args.value_of("precision") {
-            let precision = precision.parse()
-                .map_err( |_| GcalcError::ParseError("Precision should be a positive integer (usize)".to_owned()))?;
+            let precision = precision.parse().map_err(|_| {
+                GcalcError::ParseError("Precision should be a positive integer (usize)".to_owned())
+            })?;
             cal.set_precision(precision);
         }
 
@@ -216,22 +371,26 @@ impl Cli {
         Ok(())
     }
 
-    fn set_custom_column_order(cal : &mut Calculator, args: &ArgMatches) -> GcalcResult<()> {
+    fn set_custom_column_order(cal: &mut Calculator, args: &ArgMatches) -> GcalcResult<()> {
         if let Some(order) = args.value_of("column") {
             let split_orders = order.split(',').collect::<Vec<&str>>();
             let mut pair_map = HashMap::from_iter(
-                vec![ "count", "prob","cost","constant" ]
-                .into_iter().map(|v| (v.to_owned(), v.to_owned()))
+                vec!["count", "prob", "cost", "constant"]
+                    .into_iter()
+                    .map(|v| (v.to_owned(), v.to_owned())),
             );
             for pair in split_orders {
-                let pair_split : Vec<&str> = pair.split('=').collect();
+                let pair_split: Vec<&str> = pair.split('=').collect();
                 if pair_split.len() != 2 {
-                    return Err(GcalcError::InvalidArgument(format!("Could not make a column_map from given value : \"{}\"", pair)))
+                    return Err(GcalcError::InvalidArgument(format!(
+                        "Could not make a column_map from given value : \"{}\"",
+                        pair
+                    )));
                 }
 
                 let pair_type = match pair_split[0] {
-                    "count" | "prob" |"cost" | "constant" => Some(pair_split[0]),
-                    _ => None
+                    "count" | "prob" | "cost" | "constant" => Some(pair_split[0]),
+                    _ => None,
                 };
 
                 if let Some(pair_type) = pair_type {
@@ -244,14 +403,21 @@ impl Cli {
     }
 
     fn subcommand_reference() -> GcalcResult<()> {
-        std::fs::write(std::path::Path::new("ref.csv"), r#"count,probability,constant,cost"#)?;
+        std::fs::write(
+            std::path::Path::new("ref.csv"),
+            r#"count,probability,constant,cost"#,
+        )?;
         Ok(())
     }
 
     #[cfg(feature = "option")]
     fn subcommand_option() -> GcalcResult<()> {
         let path = std::path::Path::new("option.json");
-        writeln!(std::io::stdout(),"Created new option file \"{}\"", path.display())?;
+        writeln!(
+            std::io::stdout(),
+            "Created new option file \"{}\"",
+            path.display()
+        )?;
         std::fs::write(path, CalculatorOption::new().to_json()?)?;
         Ok(())
     }
